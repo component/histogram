bd8227cc24059c42b6b37c55e1e28a4fff04cae9
TJ Holowaychuk <tj@vision-media.ca>
2012-12-29 12:46:24 -0700

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,85 @@
+
+# histogram
+
+  JavaScript image histograms with Canvas.
+
+  ![js histogram](http://cdn.dropmark.com/41933/195c6ba28e2bd32341350d1acb5a50d9b4b66b4d/main.png)
+
+## Installation
+
+    $ component install component/histogram
+
+## Styling
+
+  By default the histogram will be completely black,
+  for example the following has only an opacity applied:
+
+  ![](http://cdn.dropmark.com/41933/4819d5cb323f839807bf2023969c5db875fecf9d/default-coloring.png)
+
+  Using CSS you can style the canvas histogram however you like,
+  for example the following CSS:
+
+```css
+.histogram {
+  opacity: .4;
+}
+.histogram .red {
+  color: #dc1e1a;
+}
+.histogram .green {
+  color: #12b81c;
+}
+.histogram .blue {
+  color: #46a3d6;
+}
+```
+
+ yields:
+
+   ![](http://cdn.dropmark.com/41933/3232b5147111926d36e24270de1d441d43be6317/white.png)
+
+ For the dark theme used in the first image on this page use:
+
+```css
+.histogram {
+  background: #1a1a1a;
+  opacity: .4;
+}
+.histogram .red {
+  color: #dc1e1a;
+}
+.histogram .green {
+  color: #12b81c;
+}
+.histogram .blue {
+  color: #46a3d6;
+}
+```
+
+## Scaling
+
+  The histogram's size will adjust to fit the canvas size, so use whatever dimensions you prefer:
+
+  ![](http://cdn.dropmark.com/41933/1ebf9734bf5c09e8d4b986c0eca108fe47fee459/scaled.png)
+
+## API
+
+### .smooth([n])
+
+  By default no smoothing is applied:
+
+  ![no smoothing](http://cdn.dropmark.com/41933/24c9e31a80bc86ede792aaf1dbf3d144d4e30e60/no-smooth.png)
+
+  When invoked without arguments (`.smooth()`) the
+  default kernel of `6` is used:
+
+  ![smoothing enabled](http://cdn.dropmark.com/41933/e4c31627d241db0b21e010eb72cdd853e4c9457d/smooth-default-6.png)
+
+  You may also pass any kernel size you wish, here is `20` for
+  example:
+
+  ![larger kernel](http://cdn.dropmark.com/41933/6631116b859dc420ef3c532f241e1d77af8ddb3f/smooth-20.png)
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,200 @@
+
+/**
+ * Module dependencies.
+ */
+
+var max = require('max')
+  , style = require('style')
+  , ratio = window.devicePixelRatio || 1;
+
+/**
+ * Expose `Histogram`.
+ */
+
+module.exports = Histogram;
+
+/**
+ * Initialize a new Histogram with the given reference `img`.
+ *
+ * @param {Image} img
+ * @api public
+ */
+
+function Histogram(img) {
+  if (!(this instanceof Histogram)) return new Histogram(img);
+  this.img = img;
+  this.bg = style('.histogram', 'background-color');
+  this.opacity = style('.histogram', 'opacity');
+  this.rcolor = style('.histogram .red', 'color');
+  this.gcolor = style('.histogram .green', 'color');
+  this.bcolor = style('.histogram .blue', 'color');
+}
+
+/**
+ * Set smooth kernel `size`, when invoked
+ * without a value the default of 6 is used.
+ *
+ * @param {Number} [size]
+ * @return {Histogram} self
+ * @api public
+ */
+
+Histogram.prototype.smooth = function(size){
+  switch (arguments.length) {
+    case 0:
+      this._smooth = 6;
+      break;
+    default:
+      this._smooth = size;
+  }
+
+  return this;
+};
+
+/**
+ * Draw the histogram on the given `canvas`.
+ *
+ * @param {Canvas} canvas
+ * @api public
+ */
+
+Histogram.prototype.draw = function(canvas){
+  this.canvas = canvas;
+  var w = canvas.width;
+  var h = canvas.height;
+  var ctx = canvas.getContext('2d');
+  var data = this.histogram();
+
+  var rm = max(data.r);
+  var gm = max(data.g);
+  var bm = max(data.b);
+  var m = max([rm, gm, bm]);
+
+  if (2 == ratio) ctx.scale(.5, .5);
+
+  this.barWidth = w / 256;
+
+  ctx.fillStyle = this.bg;
+  ctx.fillRect(0, 0, w, h);
+
+  ctx.fillStyle = ctx.strokeStyle = this.rcolor;
+  this.drawColor(ctx, data.r, m);
+
+  ctx.fillStyle = ctx.strokeStyle = this.gcolor;
+  this.drawColor(ctx, data.g, m);
+
+  ctx.fillStyle = ctx.strokeStyle = this.bcolor;
+  this.drawColor(ctx, data.b, m);
+};
+
+/**
+ * Plot a color range.
+ *
+ * @api private
+ */
+
+Histogram.prototype.drawColor = function(ctx, data, m){
+  var w = this.canvas.width;
+  var h = this.canvas.height;
+  var bw = this.barWidth;
+  var x = 0;
+
+  ctx.beginPath();
+  ctx.globalAlpha = this.opacity;
+  ctx.moveTo(0, h);
+
+  for (var i = 0; i < data.length; i++) {
+    var d = data[i];
+    if (this._smooth) d = this.smoothData(data, i);
+    var bh = h - h * (d / m);
+    ctx.lineTo(x += bw, bh);
+  }
+
+  ctx.lineTo(x, h);
+  ctx.fill();
+  ctx.globalAlpha = 1;
+  ctx.stroke();
+};
+
+/**
+ * Smooth the dataset.
+ *
+ * @api private
+ */
+
+Histogram.prototype.smoothData = function(data, i){
+  var k = this._smooth / 2;
+  var d = data[i];
+  for (var j = 0; j < k; j++) d += data[i - j] || 0;
+  for (var j = 0; j < k; j++) d += data[i - j] || 0;
+  return d / (k * 2 + 1);
+};
+
+/**
+ * Compute histogram data.
+ *
+ * @return {Object}
+ * @api private
+ */
+
+Histogram.prototype.histogram = function(){
+  var id = this.imageData();
+  var pixels = id.data;
+
+  var ret = {};
+  ret.r = new Array(256);
+  ret.g = new Array(256);
+  ret.b = new Array(256);
+
+  // zero
+  for (var i = 0; i < 256; i++) {
+    ret.r[i] = 0;
+    ret.g[i] = 0;
+    ret.b[i] = 0;
+  }
+
+  // fill
+  for (var i = 0; i < pixels.length; i += 4) {
+    var r = pixels[i];
+    var g = pixels[i + 1];
+    var b = pixels[i + 2];
+    ret.r[r]++;
+    ret.g[g]++;
+    ret.b[b]++;
+  }
+
+  return ret;
+};
+
+/**
+ * Return image data.
+ *
+ * @return {ImageData}
+ * @api private
+ */
+
+Histogram.prototype.imageData = function(){
+  var canvas = this.imageCanvas();
+  var ctx = canvas.getContext('2d');
+  return ctx.getImageData(0, 0, canvas.width, canvas.height);
+};
+
+/**
+ * Create the image Canvas.
+ *
+ * TODO: Draw to histogram canvas and sample from there?
+ * since we dont need the resolution this should be relative.
+ *
+ * @return {Canvas}
+ * @api private
+ */
+
+Histogram.prototype.imageCanvas = function(){
+  var img = this.img;
+  var canvas = document.createElement('canvas');
+  canvas.width = img.width;
+  canvas.height = img.height;
+  var ctx = canvas.getContext('2d');
+  ctx.drawImage(img, 0, 0);
+  return canvas;
+};
